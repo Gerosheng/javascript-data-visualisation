9cdf1556e4bc7da3fad3212def6b5c37007a2a24
Jérôme <jerome.coeck@outlook.com>
2023-11-09 10:48:48 +0100

    ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/javascript-data-visualisation-exercise/",
+    base: "/javascript-data-visualisation/",
     server: {
         proxy: {
         '/api': {
